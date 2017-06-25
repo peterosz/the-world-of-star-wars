00cbb37bac770aa0c54964a14899c65b502795ed
peterosz <itharius@gmail.com>
2017-06-25 18:31:14 +0200

    heroku db connect fix

--- connect_to_db.py
+++ connect_to_db.py
@@ -3,30 +3,26 @@ from local_config import *
 import os
 import urllib
 
-urllib.parse.uses_netloc.append('postgres')
-url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-connection = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
 
-
-def connect_db(connect_credentials):
-    conn = None
+def connect_db():
+    connection = None
     try:
-        conn = psycopg2.connect(connect_credentials)
-        return conn
+        urllib.parse.uses_netloc.append('postgres')
+        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+        connection = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+        return connection
     except Exception as error:
-        print(error)
         return 'connection error'
 
 
 def execute_query(query):
     result = {}
-    connect_credentials = "dbname={0} user={1} password={2} host={3}".format(DATABASE, USER, PASSWORD, HOST)
     connection = connect_db(connect_credentials)
     if connection == 'connection error':
         result = 'Connection error. Server unreachable.'
